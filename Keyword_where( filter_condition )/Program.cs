87285d1aa52ad794a768ff71726ceb316a36345f
Joao Vitor <joaovitoroliveirasoarespereira@gmail.com>
2024-08-13 18:49:34 -0300

    -..-...-.-.-...-.-

---
+++ Keyword_where( filter_condition )/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keyword_where__filter_condition__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
